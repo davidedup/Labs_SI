e0d8fa28668359c03816c81408c0413b7ad61b1b
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-06-21 23:39:14 -0300

    algumas funcoes

--- lab02/assets/js/main.js
+++ lab02/assets/js/main.js
@@ -3,10 +3,36 @@ var app = angular.module("lab02", []);
 app.controller("lab02Controller", function ($scope,$http) {
 	$scope.app = "Lab02";
 	$scope.series = [];
+	$scope.watchlist = [];
+	$scope.profile = [];
+
 	$scope.buscarSerie = function (serie) {
 		$http.get("https://omdbapi.com/?s="+ serie +"&apikey=93330d3c&type=series").then(function (response) {
-			$scope.series = response.data.Search;
-			delete $scope.serie;
+
+			if(response.data.Responde == "True") {
+				$scope.series = response.data.Search;
+				delete $scope.serie;
+			} else {
+				alert("A série não foi encontrada");
+			};
 		});
+	}
+
+	$scope.addSerieWatchlist = function (serie) {
+		if($scope.verificaArray(serie, $scope.watchlist)) {
+			alert("A série selecionada já está na sua Watchlist");
+		} else if ($scope.verificaArray(serie, $scope.profile)) {
+			alert("A série selecionada já está no seu perfil.")
+		} else {
+			$scope.watchlist.push(serie);
+		}
+	}
+
+	$scope.addSerieProfile = function (serie) {
+		if ($scope.verificaArray(serie, $scope.profile)) {
+			alert("A série selecionada já está no seu perfil.")
+		} else {
+			$scope.watchlist.push(serie);
+		}
 	}
 });
