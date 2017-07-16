2279180acb71f68e82f1b9f2f53ed3b6413b990c
Thalyta Fabrine <thalyta.trindade@ccc.ufcg.edu.br>
2017-07-16 18:47:17 -0300

    usuario e funcionalidades

---
+++ lab03/src/main/java/com/si1/lab03/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.si1.lab03.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.si1.lab03.model.Usuario;
+import com.si1.lab03.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+private UsuarioService usuarioService;
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/login")
+	public Usuario fazerLogin(@RequestBody Usuario usuario) {
+		usuario = usuarioService.fazerLogin(usuario);
+		return usuario;
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/")
+	public Usuario cadastrar(@RequestBody Usuario usuario) {
+		Usuario usuarioCadastrado = usuarioService.cadastrar(usuario);
+		return usuarioCadastrado;
+	}
+
+}
